b26e5372519d6829399c82bb6f7f9f33a0077c67
MischiefMicah <Micahflanery@yahoo.com>
2023-10-28 16:38:49 -0400

    initial commit

--- app.js
+++ app.js
@@ -2,75 +2,132 @@ const $ = (arg) => {
     return document.querySelector(arg)
 }
 
-const openMenu = $('.menuIcon')
-const closeMenu = $('.closeMenu')
-const menu = $('.menu')
-const shadow = $('.shadow')
-const cartIcon = $('.cartIconWrap')
-const cartMenu = $('.cartMenu')
-const quant = $('.quant')
-const decBtn = $('.decBtn')
-const incBtn = $('.incBtn')
-const addCart = $('.addCart')
-const cartQuant = $('.cartQuant')
-
-cartQuant.innerHTML != 0 ? cartQuant.classList.remove('cartQuantHide') : cartQuant.classList.add('cartQuantHide')
-
-openMenu.children[0].addEventListener('click', () => {
-    menu.classList.remove('menuHidden')
-    menu.classList.add('openMenu')
-    shadow.classList.remove('bright')
-    shadow.classList.remove('shadTransition')
-    shadow.classList.add('dark')
-})
-
-closeMenu.children[0].addEventListener('click', ()=> {
-    menu.classList.remove('openMenu')
-    menu.classList.add('menuHidden')
-    shadow.classList.remove('dark')
-    shadow.classList.add('shadTransition')
-    setTimeout(() => {
-        shadow.classList.add('bright')
-    }, 500)
-})
-
-shadow.addEventListener('click', () => {
-    menu.classList.remove('openMenu')
-    menu.classList.add('menuHidden')
-    shadow.classList.remove('dark')
-    shadow.classList.add('shadTransition')
-    setTimeout(() => {
-        shadow.classList.add('bright')
-    }, 500)
-})
-
-cartIcon.addEventListener('click', ()=> {
-    if (cartMenu.classList.contains('cartShow')) {
-        cartMenu.classList.add('cartTransition')
-        setTimeout(()=> {
-            cartMenu.classList.remove('cartShow')
-            cartMenu.classList.add('cartHide')
+const shoeApp = {
+    menuIcon: $('.menuIcon'),
+    closeMenuIcon: $('.closeMenuIcon'),
+    menu: $('.menu'),
+    shadow: $('.shadow'),
+    cartIcon: $('.cartIconWrap'),
+    cartMenu: $('.cartMenu'),
+    quant: $('.quant'),
+    decBtn: $('.decBtn'),
+    incBtn: $('.incBtn'),
+    addCartBtn: $('.addCartBtn'),
+    cartQuant: $('.cartQuant'),
+    itemWrap: $('.itemWrap'),
+    checkout: $('.checkout'),
+    item: $('.item'),
+    slides: $('.slides'),
+    slideBtns: document.querySelectorAll('.slideBtn'),
+    currSlide: $('.currSlide'),
+    images: {
+        full:[
+            'images/image-product-1.jpg',
+            'images/image-product-2.jpg',
+            'images/image-product-3.jpg',
+            'images/image-product-4.jpg'
+        ],
+        thumbnails:[
+            'images/image-product-1-thumbnail.jpg',
+            'images/image-product-2-thumbnail.jpg',
+            'images/image-product-3-thumbnail.jpg',
+            'images/image-product-4-thumbnail.jpg'
+        ]
+    },
+    openMenu: function() {
+        shoeApp.menu.classList.remove('menuHidden')
+        shoeApp.menu.classList.add('openMenu')
+        shoeApp.shadow.classList.remove('bright')
+        shoeApp.shadow.classList.remove('shadTransition')
+        shoeApp.shadow.classList.add('dark')
+    },
+    closeMenu: function() {
+        shoeApp.menu.classList.remove('openMenu')
+        shoeApp.menu.classList.add('menuHidden')
+        shoeApp.shadow.classList.remove('dark')
+        shoeApp.shadow.classList.add('shadTransition')
+        setTimeout(() => {
+            shoeApp.shadow.classList.add('bright')
         }, 500)
-    } else {
-    cartMenu.classList.remove('cartHide')
-    cartMenu.classList.remove('cartTransition')
-    cartMenu.classList.add('cartShow')
+    },
+    openCart: function() {
+        if (shoeApp.cartMenu.classList.contains('cartShow')) {
+            shoeApp.cartMenu.classList.add('cartTransition')
+            setTimeout(()=> {
+                shoeApp.cartMenu.classList.remove('cartShow')
+                shoeApp.cartMenu.classList.add('cartHide')
+            }, 500)
+        } else {
+            shoeApp.cartMenu.classList.remove('cartHide')
+            shoeApp.cartMenu.classList.remove('cartTransition')
+            shoeApp.cartMenu.classList.add('cartShow')
+        }
+    },
+    decQuant: function() {
+        shoeApp.quant.innerHTML > 0 ? shoeApp.quant.innerHTML -= 1 : shoeApp.quant.innerHTML = 0
+    },
+    incQuant: function() {
+        let tempQuant = parseInt(shoeApp.quant.innerHTML)
+        tempQuant += 1
+        shoeApp.quant.innerHTML = tempQuant
+    },
+    addItem: function() {
+        let tempCartQuant = parseInt(shoeApp.cartQuant.innerHTML)
+        let tempQuant = parseInt(shoeApp.quant.innerHTML)
+        shoeApp.cartQuant.innerHTML = tempCartQuant + tempQuant
+        if (shoeApp.cartQuant.innerHTML != 0) {
+            shoeApp.cartQuant.classList.remove('cartQuantHide')
+            shoeApp.itemWrap.children[0].classList.add('hideItem')
+            shoeApp.item.classList.remove('hideItem')
+            shoeApp.checkout.classList.remove('hideItem')
+        }
+        shoeApp.item.children[1].children[1].innerHTML = `$125.00 x ${shoeApp.cartQuant.innerHTML} <span>$${(shoeApp.cartQuant.innerHTML * 125.00).toFixed(2)}</span>`
+    },
+    delItem: function() {
+        shoeApp.itemWrap.children[0].classList.remove('hideItem')
+        shoeApp.item.classList.add('hideItem')
+        shoeApp.checkout.classList.add('hideItem')
+        shoeApp.cartQuant.classList.add('cartQuantHide')
+        shoeApp.cartQuant.innerHTML = 0
+    },
+    changeSlide: function(e) {
+        if (e.currentTarget.classList.contains('slideNext')) {
+            if (parseInt(shoeApp.currSlide.id) >= 0 && parseInt(shoeApp.currSlide.id) < 3) {
+                let tempId = parseInt(shoeApp.currSlide.id)
+                shoeApp.currSlide.id = tempId + 1
+                shoeApp.currSlide.src = shoeApp.images.full[`${shoeApp.currSlide.id}`]
+            } else if (shoeApp.currSlide.id == '3') {
+                shoeApp.currSlide.id = '0'
+                shoeApp.currSlide.src = shoeApp.images.full[`${shoeApp.currSlide.id}`]
+            }
+        }
+        if (e.currentTarget.classList.contains('slidePrev')) {
+            if (parseInt(shoeApp.currSlide.id) <= 3 && parseInt(shoeApp.currSlide.id) > 0) {
+                let tempId = parseInt(shoeApp.currSlide.id)
+                shoeApp.currSlide.id = tempId - 1
+                shoeApp.currSlide.src = shoeApp.images.full[`${shoeApp.currSlide.id}`]
+            } else if (shoeApp.currSlide.id == '0') {
+                shoeApp.currSlide.id = '3'
+                shoeApp.currSlide.src = shoeApp.images.full[`${shoeApp.currSlide.id}`]
+            }
+        }
     }
-})
+}
 
-decBtn.addEventListener('click', ()=>{
-    quant.innerHTML > 0 ? quant.innerHTML -= 1 : quant.innerHTML = 0
-})
+shoeApp.cartQuant.innerHTML != 0 ? shoeApp.cartQuant.classList.remove('cartQuantHide') : shoeApp.cartQuant.classList.add('cartQuantHide')
 
-incBtn.addEventListener('click', ()=>{
-    let tempQuant = parseInt(quant.innerHTML)
-    tempQuant += 1
-    quant.innerHTML = tempQuant
-})
+shoeApp.menuIcon.children[0].addEventListener('click', shoeApp.openMenu)
+shoeApp.closeMenuIcon.children[0].addEventListener('click', shoeApp.closeMenu)
+shoeApp.shadow.addEventListener('click', shoeApp.closeMenu)
 
-addCart.addEventListener('click',()=> {
-    let tempCartQuant = parseInt(cartQuant.innerHTML)
-    let tempQuant = parseInt(quant.innerHTML)
-    cartQuant.innerHTML = tempCartQuant + tempQuant
-    cartQuant.innerHTML != 0 ? cartQuant.classList.remove('cartQuantHide') : cartQuant.classList.add('cartQuantHide')
+shoeApp.cartIcon.addEventListener('click', shoeApp.openCart)
+
+shoeApp.decBtn.addEventListener('click', shoeApp.decQuant)
+shoeApp.incBtn.addEventListener('click', shoeApp.incQuant)
+
+shoeApp.addCartBtn.addEventListener('click', shoeApp.addItem)
+shoeApp.item.children[2].addEventListener('click', shoeApp.delItem)
+
+shoeApp.slideBtns.forEach((btn) => {
+    btn.addEventListener('click', shoeApp.changeSlide)
 })
